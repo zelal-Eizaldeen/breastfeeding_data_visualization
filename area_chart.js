88b7e253eb7b59986e55ca1affd38d8640c56410
Zelal EizAldeen <zelalezaldeen@gmail.com>
2023-07-26 09:38:15 +0300

    Finish all scenese

--- area_chart.js
+++ area_chart.js
@@ -13,25 +13,35 @@ desBtn.disabled=true;
 
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 30, bottom: 40, left: 90},
-    width = 460 - margin.left - margin.right,
+    width = 600 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
+    // var margin = {top:20, right:20, bottom:70, left:70};
+    // var width = 600 - margin.left - margin.right;
+    // var height = 600 - margin.top - margin.bottom;
 
- 
 
 ///////////Click ///////////
 d3.selectAll(".front").on("click", function () {
    if (this.value=="asc"){
     desBtn.disabled=false;
     ascBtn.disabled=true;
-
+   
+  
    }
    if(this.value=="des"){
     ascBtn.disabled=false;
     desBtn.disabled=true;
 
    }
+   if(type=="Exclusive"){
     d3.select("svg").remove();
     sort_exclusive(this.value)
+   }
+   if(type=="Ever"){
+    d3.select("svg").remove();
+    sort_ever(this.value);
+   }
+   
    
 
 });
@@ -58,7 +68,6 @@ d3.selectAll(".region_cb").on("change", function ()
   }
   if (this.checked && type=="Ever") { // adding data points
  
-   console.log(type)
     stateExCheckbox.checked=false;
     d3.select("svg").remove();
     init_ever(type)
@@ -69,6 +78,7 @@ d3.selectAll(".region_cb").on("change", function ()
 var div = d3.select("body").append("div")
 .attr("class", "tooltip")
 .style("opacity", 0);
+
 var fill_bars = function(d,i,n){
     if (type=="Exclusive"){
         d3.select(n[i]).style("fill", "#57B795")
@@ -77,10 +87,6 @@ var fill_bars = function(d,i,n){
         d3.select(n[i]).style("fill", "#798BBC")
     }
 }
-
-   
-
-
 
   var mouseover = function(d,i,n){
     d3.select(n[i])
@@ -103,7 +109,6 @@ var fill_bars = function(d,i,n){
            
             </p>
             `
- 
             )
                 .style("left", (d3.event.pageX+40) + "px")
                 .style("top", (d3.event.pageY-140) + "px")
@@ -137,8 +142,6 @@ var fill_bars = function(d,i,n){
    .duration(500)
    .style("opacity", 0)
   
-
-   
       }
 
           
@@ -149,6 +152,7 @@ async function init_ex(type) {
     // append the svg object to the body of the page
  const svg = d3.select("#canva")
  .append("svg")
+      
    .attr("width", width + margin.left + margin.right)
    .attr("height", height + margin.top + margin.bottom)
  .append("g")
@@ -157,14 +161,8 @@ async function init_ex(type) {
 
         var nodes = d3.range(data.length)
         .map(function (d) {
-                          let i = + data[d].category_code_Mum;
                           d={
-                            cluster: i,
-                            Outcomes_Mum: data[d].Outcomes_Mum,
-                            Lower_Risk_Mum: data[d].Lower_Risk_Mum,
-                            Breastfeeding_Period_Mum:data[d].Breastfeeding_Period_Mum,
                             Country:data[d].Country,
-                            Ever: data[d].Ever,
                             Exclusive: data[d].Exclusive,
                             
                           }
@@ -182,8 +180,6 @@ svg.append("g")
   .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
     .style("text-anchor", "end");
-
-  
      // Y axis
   var y = d3.scaleBand()
   .range([ 0, height ])
@@ -196,29 +192,29 @@ svg.append("g")
   
 
 //Annotation
- const annotations = [
-  {
+const annotations = [
+    {
     note: {
-      label: "Gestational Diabetes Mellitus reduced to 78%",
-      title: "GDM:",
-      align: "left",
-      wrap: 100,
-      
+    label: "The highest percentage, about 36.5% in 2019. Why?",
+    title: "Minnesota has",
+    align: "left",
+    wrap: 100,
+    
     },
     connector: {
-      end: "dot",        // Can be none, or arrow or dot
-      type: "line",      // ?? don't know what it does
-      lineType : "vertical",    // ?? don't know what it does
-      endScale: 10     // dot size
+    end: "dot",        // Can be none, or arrow or dot
+    type: "line",      // ?? don't know what it does
+    lineType : "vertical",    // ?? don't know what it does
+    endScale: 10     // dot size
     },
     color: ["#000000"],
     
-    x: width/3,
-    y: height/2.7,
+    x: width/2.2,
+    y: height/70,
     dy: 60,
     dx: 140
-  }
-]
+    }
+    ]
    // Add annotation to the baby chart
    const makeAnnotations = d3.annotation()
    .annotations(annotations);
@@ -228,7 +224,6 @@ svg.append("g")
   // variable u: map data to existing bars
 var exclusive_rects = svg.selectAll("rect")
 .data(nodes)
-
 // update bars
 exclusive_rects
 .enter()
@@ -239,9 +234,6 @@ exclusive_rects
 .attr("x", x(0) )
 .attr("y", function(d) { return y(d.Country); })
 .attr("width", function(d) { return x(d[type]);; })  //Desc
-// .attr("width",  function(d,i) { return x(exclusive_arr_asc[i]); })  //Asc
-
-
  .attr("height", y.bandwidth() )
 .attr("fill",function(d,i,n) { return fill_bars(d,i,n); })
 //Annotations
@@ -249,18 +241,20 @@ svg.append("g")
 .attr("class", "annotation-group")
 .call(makeAnnotations)
 .transition().duration(2000).delay(500)
-                      .style("opacity", 1);
+                      .style("opacity", 1)
+                    
 
 }
 
+
 async function init_ever(type) {
      data = await d3.csv(
-        'Breastfeeding_Outcomes.csv');
+        'Breastfeeding_OutcomesEverDes.csv');
 
 // append the svg object to the body of the page
 const svg = d3.select("#canva")
 .append("svg")
-  .attr("width", width + margin.left + margin.right)
+  .attr("width", 1000)
   .attr("height", height + margin.top + margin.bottom)
 .append("g")
   .attr("transform",
@@ -268,16 +262,10 @@ const svg = d3.select("#canva")
 
         var nodes = d3.range(data.length)
         .map(function (d) {
-                          let i = + data[d].category_code_Mum;
                           d={
-                            cluster: i,
-                            Outcomes_Mum: data[d].Outcomes_Mum,
-                            Lower_Risk_Mum: data[d].Lower_Risk_Mum,
-                            Breastfeeding_Period_Mum:data[d].Breastfeeding_Period_Mum,
                             Country:data[d].Country,
                             Ever: data[d].Ever,
-                            Exclusive: data[d].Exclusive,
-                            
+      
                           }
                      return d;
                       });
@@ -310,8 +298,8 @@ svg.append("g")
  const annotations = [
   {
     note: {
-      label: "Gestational Diabetes Mellitus reduced to 78%",
-      title: "GDM:",
+      label: "The vast majority of mothers had any amount of breastfeeding",
+      title: "Colorado",
       align: "left",
       wrap: 100,
       
@@ -324,8 +312,8 @@ svg.append("g")
     },
     color: ["#000000"],
     
-    x: width/3,
-    y: height/2.7,
+    x: width,
+    y: height/60,
     dy: 60,
     dx: 140
   }
@@ -335,14 +323,6 @@ svg.append("g")
    .annotations(annotations);
 
 
-////// Sorting ///////
-
-ever_arr_asc=[]
-if (ascBtn.value=="asc"){
-    for(let i=0; i < nodes.length; i ++) {
-       ever_arr_asc.push(nodes[i].Ever)
-       ever_arr_asc.sort(d3.descending)
-    }
      // variable u: map data to existing bars
    var ever_rects = svg.selectAll("rect")
    .data(nodes)
@@ -354,15 +334,12 @@ if (ascBtn.value=="asc"){
    .merge(ever_rects)
    .transition()
    .duration(1000)
-  
-
    .attr("x", x(0) )
    .attr("y", function(d) { return y(d.Country); })
-//    .attr("width", function(d) { return x(d[type]);; })  //Desc
-     .attr("width",  function(d,i) { return x(ever_arr_asc[i]); })  //Asc
+.attr("width", function(d) { return x(d[type]); })  //Desc
    .attr("height", y.bandwidth() )
    .attr("fill",function(d,i,n) { return fill_bars(d,i,n); })
-}
+
 
  //Annotations
  svg.append("g")
@@ -374,6 +351,7 @@ if (ascBtn.value=="asc"){
 }
             
 async function sort_exclusive(order) {
+    
     const data = await d3.csv(
             'Breastfeeding_Outcomes.csv');
      // append the svg object to the body of the page
@@ -384,18 +362,17 @@ async function sort_exclusive(order) {
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
- 
-         var nodes = d3.range(data.length)
+////Sort Exclusive Ascending ///////
+if(order==="asc"){
+            sortedData_asc = data.slice()
+            .sort((a, b) => d3.ascending(a.Exclusive, b.Exclusive))
+            
+         var nodes = d3.range(sortedData_asc.length)
          .map(function (d) {
-                           let i = + data[d].category_code_Mum;
+                           let i = + sortedData_asc[d].category_code_Mum;
                            d={
-                             cluster: i,
-                             Outcomes_Mum: data[d].Outcomes_Mum,
-                             Lower_Risk_Mum: data[d].Lower_Risk_Mum,
-                             Breastfeeding_Period_Mum:data[d].Breastfeeding_Period_Mum,
-                             Country:data[d].Country,
-                             Ever: data[d].Ever,
-                             Exclusive: data[d].Exclusive,
+                             Country:sortedData_asc[d].Country,
+                             Exclusive: sortedData_asc[d].Exclusive,
                              
                            }
                       return d;
@@ -403,7 +380,7 @@ async function sort_exclusive(order) {
     
  // Add X axis
  var x = d3.scaleLinear()
- .domain([0,d3.max(data, d=>+d[type])])
+ .domain([0,d3.max(sortedData_asc, d=>+d[type])])
    .range([ 0, width]);
  svg.append("g")
    .attr("transform", "translate(0," + height + ")")
@@ -417,7 +394,7 @@ async function sort_exclusive(order) {
       // Y axis
    var y = d3.scaleBand()
    .range([ 0, height ])
-   .domain(data.map(d =>d.Country))
+   .domain(sortedData_asc.map(d =>d.Country))
  
    .padding(.1);
  svg.append("g")
@@ -429,8 +406,8 @@ async function sort_exclusive(order) {
   const annotations = [
    {
      note: {
-       label: "Gestational Diabetes Mellitus reduced to 78%",
-       title: "GDM:",
+       label: "The Lowest percentage, about 13.8% in 2019",
+       title: "West Virginia has",
        align: "left",
        wrap: 100,
        
@@ -443,8 +420,8 @@ async function sort_exclusive(order) {
      },
      color: ["#000000"],
      
-     x: width/3,
-     y: height/2.7,
+     x: width/2.5,
+     y: height/50,
      dy: 60,
      dx: 140
    }
@@ -452,25 +429,8 @@ async function sort_exclusive(order) {
     // Add annotation to the baby chart
     const makeAnnotations = d3.annotation()
     .annotations(annotations);
- 
- 
-  exclusive_arr_asc=[]
-       if(order==="asc"){
-    for(let i=0; i < nodes.length; i ++) {
-     exclusive_arr_asc.push(nodes[i].Exclusive)
-     exclusive_arr_asc.sort(d3.ascending)
-  }
-}
 
-if(order==="des"){
-    for(let i=0; i < nodes.length; i ++) {
-     exclusive_arr_asc.push(nodes[i].Exclusive)
-     exclusive_arr_asc.sort(d3.descending)
-  }
-}
-
- 
-   // variable u: map data to existing bars
+     // variable u: map data to existing bars
  var exclusive_rects = svg.selectAll("rect")
  .data(nodes)
  
@@ -483,10 +443,7 @@ if(order==="des"){
  .duration(1000)
  .attr("x", x(0) )
  .attr("y", function(d) { return y(d.Country); })
-//  .attr("width", function(d) { return x(d[type]);; })  //Desc
- .attr("width",  function(d,i) { return x(exclusive_arr_asc[i]); })  //Asc
- 
- 
+ .attr("width", function(d) { return x(d[type]); })  //Desc
   .attr("height", y.bandwidth() )
  .attr("fill",function(d,i,n) { return fill_bars(d,i,n); })
  //Annotations
@@ -496,16 +453,302 @@ if(order==="des"){
  .transition().duration(2000).delay(500)
                        .style("opacity", 1);
  
- }
-
-
-
-////// Sorting ///////
-
-
    
+}
+ 
+     
+///////Sort Exclusive Descending
+if(order==="des"){
+    sortedData_des = data.slice()
+    .sort((a, b) => d3.descending(a.Exclusive, b.Exclusive))
+    
+ var nodes = d3.range(sortedData_des.length)
+ .map(function (d) {
+                   d={
+                     Country:sortedData_des[d].Country,
+                     Exclusive: sortedData_des[d].Exclusive,
+                     
+                   }
+              return d;
+               });
+
+// Add X axis
+var x = d3.scaleLinear()
+.domain([0,d3.max(sortedData_des, d=>+d[type])])
+.range([ 0, width]);
+svg.append("g")
+.attr("transform", "translate(0," + height + ")")
+.transition().duration(1000)
+.call(d3.axisBottom(x))
+.selectAll("text")
+.attr("transform", "translate(-10,0)rotate(-45)")
+.style("text-anchor", "end");
 
 
+// Y axis
+var y = d3.scaleBand()
+.range([ 0, height ])
+.domain(sortedData_des.map(d =>d.Country))
 
+.padding(.1);
+svg.append("g")
+.transition().duration(1000)
+.call(d3.axisLeft(y)) 
+
+
+//Annotation
+const annotations = [
+{
+note: {
+label: "The highest percentage, about 36.5% in 2019. Why?",
+title: "Minnesota has",
+align: "left",
+wrap: 100,
+
+},
+connector: {
+end: "dot",        // Can be none, or arrow or dot
+type: "line",      // ?? don't know what it does
+lineType : "vertical",    // ?? don't know what it does
+endScale: 10     // dot size
+},
+color: ["#000000"],
+
+x: width/2.2,
+y: height/70,
+dy: 60,
+dx: 140
+}
+]
+// Add annotation to the baby chart
+const makeAnnotations = d3.annotation()
+.annotations(annotations);
+
+// variable u: map data to existing bars
+var exclusive_rects = svg.selectAll("rect")
+.data(nodes)
+
+// update bars
+exclusive_rects
+.enter()
+.append("rect")
+.merge(exclusive_rects)
+.transition()
+.duration(1000)
+.attr("x", x(0) )
+.attr("y", function(d) { return y(d.Country); })
+.attr("width", function(d) { return x(d[type]); })  
+.attr("height", y.bandwidth() )
+.attr("fill",function(d,i,n) { return fill_bars(d,i,n); })
+//Annotations
+svg.append("g")
+.attr("class", "annotation-group")
+.call(makeAnnotations)
+.transition().duration(2000).delay(500)
+               .style("opacity", 1);
+
+}
+}
+////////Sort Ever///
+async function sort_ever(order) {
+    
+    const data = await d3.csv(
+            'Breastfeeding_OutcomesEverDes.csv');
+     // append the svg object to the body of the page
+  const svg = d3.select("#canva")
+  .append("svg")
+  .attr("width", 1000)
+
+    // .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+////Sort Ever Ascending ///////
+if(order==="asc"){
+            sortedData_asc = data.slice()
+            .sort((a, b) => d3.ascending(a.Ever, b.Ever))
+            
+         var nodes = d3.range(sortedData_asc.length)
+         .map(function (d) {
+                           d={
+                             Country:sortedData_asc[d].Country,
+                             Ever: sortedData_asc[d].Ever,
+                             
+                           }
+                      return d;
+                       });
+    
+ // Add X axis
+ var x = d3.scaleLinear()
+ .domain([0,d3.max(sortedData_asc, d=>+d[type])])
+   .range([ 0, width]);
+ svg.append("g")
+   .attr("transform", "translate(0," + height + ")")
+   .transition().duration(1000)
+   .call(d3.axisBottom(x))
+   .selectAll("text")
+     .attr("transform", "translate(-10,0)rotate(-45)")
+     .style("text-anchor", "end");
+ 
+   
+      // Y axis
+   var y = d3.scaleBand()
+   .range([ 0, height ])
+   .domain(sortedData_asc.map(d =>d.Country))
+ 
+   .padding(.1);
+ svg.append("g")
+ .transition().duration(1000)
+   .call(d3.axisLeft(y)) 
+   
+ 
+ //Annotation
+  const annotations = [
+   {
+     note: {
+       label: "The Lowest percentage, about 60%(breastfeeding and Formula) in 2019",
+       title: "West Virginia has",
+       align: "left",
+       wrap: 100,
+       
+     },
+     connector: {
+       end: "dot",        // Can be none, or arrow or dot
+       type: "line",      // ?? don't know what it does
+       lineType : "vertical",    // ?? don't know what it does
+       endScale: 10     // dot size
+     },
+     color: ["#000000"],
+     
+     x: width/1.5,
+     y: height/50,
+     dy: 60,
+     dx: 140
+   }
+ ]
+    // Add annotation to the baby chart
+    const makeAnnotations = d3.annotation()
+    .annotations(annotations);
+
+     // variable u: map data to existing bars
+ var ever_rects = svg.selectAll("rect")
+ .data(nodes)
+ 
+ // update bars
+ ever_rects
+ .enter()
+ .append("rect")
+ .merge(ever_rects)
+ .transition()
+ .duration(1000)
+ .attr("x", x(0) )
+ .attr("y", function(d) { return y(d.Country); })
+ .attr("width", function(d) { return x(d[type]); })  //Desc
+  .attr("height", y.bandwidth() )
+ .attr("fill",function(d,i,n) { return fill_bars(d,i,n); })
+ //Annotations
+ svg.append("g")
+ .attr("class", "annotation-group")
+ .call(makeAnnotations)
+ .transition().duration(2000).delay(500)
+                       .style("opacity", 1);
+ 
+   
+}
+ 
+     
+///////Sort Ever Descending
+if(order==="des"){
+    sortedData_des = data.slice()
+    .sort((a, b) => d3.descending(a.Ever, b.Ever))
+    
+ var nodes = d3.range(sortedData_des.length)
+ .map(function (d) {
+                   d={
+                     Country:sortedData_des[d].Country,
+                     Ever: sortedData_des[d].Ever,
+                     
+                   }
+              return d;
+               });
+
+// Add X axis
+var x = d3.scaleLinear()
+.domain([0,d3.max(sortedData_des, d=>+d[type])])
+.range([ 0, width]);
+svg.append("g")
+.attr("transform", "translate(0," + height + ")")
+.transition().duration(1000)
+.call(d3.axisBottom(x))
+.selectAll("text")
+.attr("transform", "translate(-10,0)rotate(-45)")
+.style("text-anchor", "end");
+
+
+// Y axis
+var y = d3.scaleBand()
+.range([ 0, height ])
+.domain(sortedData_des.map(d =>d.Country))
+
+.padding(.1);
+svg.append("g")
+.transition().duration(1000)
+.call(d3.axisLeft(y)) 
+
+
+//Annotation
+const annotations = [
+    {
+      note: {
+        label: "The vast majority of mothers had any amount of breastfeeding",
+        title: "Colorado",
+        align: "left",
+        wrap: 100,
+        
+      },
+      connector: {
+        end: "dot",        // Can be none, or arrow or dot
+        type: "line",      // ?? don't know what it does
+        lineType : "vertical",    // ?? don't know what it does
+        endScale: 10     // dot size
+      },
+      color: ["#000000"],
+      
+      x: width,
+      y: height/60,
+      dy: 60,
+      dx: 140
+    }
+  ]
+// Add annotation to the baby chart
+const makeAnnotations = d3.annotation()
+.annotations(annotations);
+
+// variable u: map data to existing bars
+var ever_rects = svg.selectAll("rect")
+.data(nodes)
+
+// update bars
+ever_rects
+.enter()
+.append("rect")
+.merge(ever_rects)
+.transition()
+.duration(1000)
+.attr("x", x(0) )
+.attr("y", function(d) { return y(d.Country); })
+.attr("width", function(d) { return x(d[type]); })  
+.attr("height", y.bandwidth() )
+.attr("fill",function(d,i,n) { return fill_bars(d,i,n); })
+//Annotations
+svg.append("g")
+.attr("class", "annotation-group")
+.call(makeAnnotations)
+.transition().duration(2000).delay(500)
+               .style("opacity", 1);
+
+}
+}
 
 init_ex(type)
